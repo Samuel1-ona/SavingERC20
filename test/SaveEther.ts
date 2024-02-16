5545e8a0f061705fc0efc463b40dbdb6e608dbd8
Samuel1-ona <samuonanikesamuel123@gmail.com>
2024-02-16 10:50:29 +0100

    deposit testing

--- test/SaveEther.ts
+++ test/SaveEther.ts
@@ -1,21 +1,42 @@
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-
-import { expect, assert} from "chai";
+import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("SaveERC20", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deploySaveEther() {
-    // Contracts are deployed using the first signer/account by default
+  async function deploySaveERC20() {
     const [owner, account1, account2] = await ethers.getSigners();
 
     const SaveERC20 = await ethers.getContractFactory("SaveERC20");
     const saveERC20 = await SaveERC20.deploy(owner.address);
-    const amountToDeposit = ethers.parseEther("5");
+    const amountToDeposit = ethers.parseEther("100000");
 
     return { saveERC20, owner, account1, account2, amountToDeposit };
   }
 
-  it()
+  describe("Checking contract balance", function () {
+    it("should withdraw tokens to the owner", async function () {
+      const { saveERC20, owner, amountToDeposit } = await loadFixture(deploySaveERC20);
+
+      it("should deposit tokens into the contract", async function () {
+        const savingToken = "0x925c814D19000E9686eAd37B5dB3cf0815622C9E" 
+        const amountToDeposit = ethers.parseEther("5");
+        
+        // Approve SaveERC20 contract to transfer tokens
+        const Token = await ethers.getContractAt("IERC20", savingToken);
+        await Token.approve(owner.address, amountToDeposit);
+    
+        // Deposit tokens into the contract
+        await saveERC20.deposit(amountToDeposit);
+    
+        // Check if the user's savings have increased
+        const userBalance = await saveERC20.checkUserBalance(owner.address);
+        expect(userBalance).to.equal(amountToDeposit);
+    
+        // Check if the contract's balance matches the deposited amount
+        const contractBalance = await saveERC20.checkContractBalance();
+        expect(contractBalance).to.equal(amountToDeposit);
+      });
+    
+    });
+  });
+});
